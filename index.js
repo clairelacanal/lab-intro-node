5c963940761dbccd83d6c835f60efec8a6bffb7e
Marwa Iben Khalifa <marwa.brj@gmail.com>
2020-08-06 22:06:14 +0200

    ite 4 et 5 done

--- index.js
+++ index.js
@@ -20,9 +20,21 @@ class SortedList{
     }
   }
 
-  max() {}
+  max() {
+    if(this.length> 0){
+      return this.items[this.length-1]
+    }else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length> 0){
+      return this.items[0]
+    }else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
   sum() {}
 
