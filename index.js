ba6ce951d6ab88edfe0a9fcf523f4417ba133115
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-29 16:19:40 +0200

    lab solved

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=> a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.items.length ||pos < 0 ) {
+      throw new Error('OutOfBounds'); 
+    }
+    return this.items[pos]; 
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');  
+    }
+    let big = this.items[0];  
+    for (let i = 1; i < this.items.length; i++) {  
+      if (this.items[i] > big) {
+        big = this.items[i]; 
+      }
+    }
+    return big;  
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList'); 
+    }
+    let minimum = this.items[0];
+    for(let i = 1; i < this.items.length; i++){
+      if(this.items[i] < minimum){
+        minimum = this.items[i];
+      }
+    }
+    return minimum;
+  }
 
   sum() {}
 
